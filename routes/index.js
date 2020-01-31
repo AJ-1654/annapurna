81f905d0bac9619924d26de01517cee0816f43f6
Mansi Joshi <mansij1999@gmail.com>
2020-01-31 19:19:34 +0530

    Third commits

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('registerUser');
+  res.render('NGOLanding');
 });
 
 module.exports = router;
