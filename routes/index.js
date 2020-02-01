70880e3035cdad9c089670acdca7918e02986da4
msknkh <muskankhandelwal369@gmail.com>
2020-02-01 11:33:19 +0530

    Beautification

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ngoNearMe');
+  res.render('UserLanding');
 });
 
 module.exports = router;
